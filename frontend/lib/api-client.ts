cb0092610569656fad98f908e671df2af3377382
robel <robelyinager@gmail.com>
2026-07-08 07:16:31 +0300

    add axios api client

---
+++ frontend/lib/api-client.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  headers: { "Content-Type": "application/json" },
+});
